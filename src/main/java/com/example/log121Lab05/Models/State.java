a89062e3fee4520592a757f94174c75db28fa64f
Yvon Meunier <meunieryvon6@gmail.com>
2025-08-01 23:14:03 -0400

    added file doc header template

--- src/main/java/com/example/log121Lab05/Models/State.java
+++ src/main/java/com/example/log121Lab05/Models/State.java
@@ -1,3 +1,11 @@
+/*
+ * @Author: mikey.zhaopeng
+ * @Email:  admin@example.com
+ * @Date:   2016-07-29 15:57:29
+ * @Last Modified by: Noscere
+ * @Last Modified time: 2022-11-15 06:25:53.546
+ * @Description: description
+ */
 package com.example.log121Lab05.Models;
 
 import com.example.log121Lab05.Models.Commands.PasteType;
